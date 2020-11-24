76f8ecf096345437fa29fc0bfbb5a7745d821f40
patriciomirosh <71236425+patriciomirosh@users.noreply.github.com>
2020-11-24 19:09:53 -0300

    Update Mails.py

--- Mails.py
+++ Mails.py
@@ -5,14 +5,13 @@ from email.mime.base import MIMEBase
 from email import encoders
 from decouple import config
 import pandas as pd
-
+# This part i enclose the file where i have the mails to send.
 df = pd.read_excel('correos.xlsx', sheet_name='Table 1')
 df
 dfem=df.loc[:, ['empresas', 'mails']]
 dfem.head(30)
-# Iniciamos los parámetros del script
-
-for i in range(424,len(dfem)):
+# Inicializate the scripts:
+for i in range(0,len(dfem)):
     remitente = 'mirospatricio@gmail.com'
     try:
         destinatarios = dfem['mails'][i]
@@ -20,25 +19,23 @@ for i in range(424,len(dfem)):
         print('Encoding error')
     asunto = 'Ref: Me postulo para trabajar'
     
-    cuerpo = '''Estimados {}: 
-Mi nombre es Miroshnitshenko Patricio, si me regalan unos segundos les comento mi situación:  me recibí recientemente de Ingeniero Químico, pero no poseo experiencia profesional aunque sí experiencia en el área de ventas y facturación en pintureria, por esta razón estoy en la búsqueda activa y predispuesto a trabajar sin remuneracion con el objetivo de aprender, obtener experiencia y ofrecer mis conocimientos. 
-Estoy dispuesto a trabajar en cualquier área,  tengo conocimientos en muchos programas informaticos como: hysys y Dwsim y se programar en python. Le adjunto mi CV y mi carta de presentación.
-Saludos cordiales.  '''.format(dfem['empresas'][i])
+    cuerpo = '''Dear {}: 
+I send this mail for make contact with our sales department and provide my curriculum, i have experienced in online sales .  '''.format(dfem['empresas'][i])
     ruta_adjunto = 'CV_miroshnitshenko_Patricio_Ing_3010.pdf'
     nombre_adjunto = 'Curriculo Miroshnitshenko Patricio'
 
-    # Creamos el objeto mensaje
+    # Create the multipart objecto 
     mensaje = MIMEMultipart()
     
-    # Establecemos los atributos del mensaje
+    # we place the attributes of message 
     mensaje['From'] = remitente
     mensaje['To'] = destinatarios
     mensaje['Subject'] = asunto
     
-    # Agregamos el cuerpo del mensaje como objeto MIME de tipo texto
+    # add the body of the message at the mime object
     mensaje.attach(MIMEText(cuerpo, 'plain'))
     
-    # Abrimos el archivo que vamos a adjuntar
+    # Open the files we wish to enclose
 
     pdfname = 'CV_miroshnitshenko_Patricio_Ing_3010.pdf'
     pdfname2= 'Carta_de_presentacion.pdf'
@@ -65,23 +62,23 @@ Saludos cordiales.  '''.format(dfem['empresas'][i])
 
 
 
-    # Creamos la conexión con el servidor
+    # Create the conexion to the server
     sesion_smtp = smtplib.SMTP('smtp.gmail.com', 587)
     
-    # Ciframos la conexión
+    # Encoding the conection
     sesion_smtp.starttls()
 
-    # Iniciamos sesión en el servidor
+    # Starting the server conexion
     SECRET_KEY = config('SECRET_KEY')
     sesion_smtp.login('mirospatricio@gmail.com',SECRET_KEY)
 
-    # Convertimos el objeto mensaje a texto
+    # we convert the object message in to text
     texto = mensaje.as_string()
 
-    # Enviamos el mensaje
-    try:
+    # Send the message
+    try: # try to catch encode error as '-' in the mails, 
         sesion_smtp.sendmail(remitente, destinatarios, texto)
     except:
         print('Encode Error ')
-    # Cerramos la conexión
+    #We close the conextion
     sesion_smtp.quit()
